cb83ef32b711b05b97c44fe48e6c6f018789f6a0
Adam Weingram <adamweingram@gmail.com>
2021-02-05 02:14:58 -0800

    Removed unused member variable in base Dataset class

--- raster_pack/dataset/dataset.py
+++ raster_pack/dataset/dataset.py
@@ -7,7 +7,6 @@ from typing import Optional, Dict
 class Dataset:
     profile: rio.profiles.Profile
     bands: Dict[str, np.ndarray]
-    original_path: Optional[str]
     meta: Optional[dict]
 
     def __init__(self, profile: rio.profiles.Profile, bands: Dict[str, np.ndarray], meta: Optional[dict]):
